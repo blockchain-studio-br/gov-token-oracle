24cabe3e3f6e2884a2fdcf97f00fe80b0cc3fca9
Fabiano <fabiano.paula.martins@gmail.com>
2019-06-24 17:10:40 -0300

    Cleaning project

--- app.js
+++
@@ -1,9 +0,0 @@
-var http = require('http');
-var app = require('./config/express');
-var port = 3000;
-
-http
-	.createServer(app)
-	.listen(port, () => {
-	    console.log("Server Initialized on port " + port);
-	});
--- app/routes/main.js
+++
@@ -1,9 +0,0 @@
-module.exports = function(app) {
- 	
- 	var api = app.api;
-
-    app
-    	.route('/')
-    	.get(api.validator.index);
-
-};
