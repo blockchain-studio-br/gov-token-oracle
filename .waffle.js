2c2a7bfd99b8597d10aca6c1a8f0e765d2e0384c
Fabiano <fabiano.paula.martins@gmail.com>
2019-07-02 15:08:38 -0300

    Setup configuration

---
+++ .waffle.js
@@ -0,0 +1,7 @@
+module.exports = new Promise((resolve, reject) => {
+	resolve({
+		"sourcesPath": "./govtoken-contracts/contracts",
+		"solcVersion": "v0.5.3+commit.10d17f24",
+		"legacyOutput": "true"
+	});
+});
